f2cd8bbea4fe206f797237dc05587db29b97bfa3
Pedro Nascimento <sn.jaypee@gmail.com>
2023-11-13 04:14:15 -0300

    fix: remove unused function

--- src/repositories/games.repository.ts
+++ src/repositories/games.repository.ts
@@ -8,13 +8,6 @@ async function ReadAll(): Promise<Game[]> {
     return result;
 }
 
-async function CheckExistence(id: number) {
-    const result = await prisma.bet.findUnique({ where: { id } });
-
-    if (!result) return false;
-    return true;
-}
-
 async function ReadById(id: number): Promise<Game & { Bet: Bet[] }> {
     try {
         const result = await prisma.game.findUniqueOrThrow({ where: { id }, include: { Bet: true } });
@@ -49,7 +42,6 @@ async function Finish(id: number, homeTeamScore: number, awayTeamScore: number):
 
 export const GamesRepository = {
     ReadAll,
-    CheckExistence,
     ReadById,
     Create,
     Finish,
